764e82d04b8295f2cb986b2b6d08db61e30cdd42
Ritesh381 <prajapatiritesh381@gmail.com>
2025-03-28 12:49:56 +0530

    Made vertical view responsive

--- src/components/VerticalView.jsx
+++ src/components/VerticalView.jsx
@@ -3,6 +3,7 @@ import MovieCard from "./MovieCard";
 
 function VerticalView({ movies }) {
   const [loading, setLoading] = useState(true);
+  
   useEffect(() => {
     if (movies.length > 0) {
       setLoading(false);
@@ -10,9 +11,10 @@ function VerticalView({ movies }) {
       setLoading(true);
     }
   }, [movies]);
+  
   return (
     <div>
-      <div className="grid grid-cols-5 gap-6">
+      <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-5 gap-6">
         {loading ? (
           <div className="text-white text-lg">Loading...</div>
         ) : (
