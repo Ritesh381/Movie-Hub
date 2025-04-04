bee74144b462331a7effc04ac3b38ee92fbcb1b2
Ritesh381 <prajapatiritesh381@gmail.com>
2025-04-05 01:07:37 +0530

    added encironment variables

--- src/assets/key.js
+++ src/assets/key.js
@@ -1,5 +1,2 @@
-export const API_KEY = "6afe036d391f25fd70b37bbb83def578"
-export const GEMINI_API_KEY = "AIzaSyAARlfUAU5gNVlUAa6WdMUK_QyiZLVI6qY"
-export const DEEPSEEK_API_KEY = "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"
-
-// Change the api keys with environment variables
+export const API_KEY = import.meta.env.VITE_TMDB_API_KEY || ""
+export const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY || ""
